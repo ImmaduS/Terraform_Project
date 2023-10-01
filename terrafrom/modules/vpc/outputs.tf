a4936401342b4cf912ecda3a9a2c3895f2b2c3b8
Amadoudiallo2 <diallo.amadousadjo@yahoo.fr>
2023-10-01 17:25:17 -0400

    update vpc

--- terrafrom/modules/vpc/outputs.tf
+++ terrafrom/modules/vpc/outputs.tf
@@ -1,27 +1,6 @@
+# To display the outputs for the variable in question
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+output "vpc_id" {
+  description = "The VPC Identifier"
+  value = "aws_vpc.exemple.id"
+}
